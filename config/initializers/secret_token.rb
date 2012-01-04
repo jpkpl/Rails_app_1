36f14c5c25ad3dc2a5831aa9e926060be5b6c96f
unknown <Utilisateur@jp-2.(none)>
2012-01-04 12:49:10 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testjp::Application.config.secret_token = 'c807c4626c3e3fc275ed5dc562b9602f899d20924eb8903670cebee80c72946343ece8ec30506b3db7025508b601635a412a73564f2f1172d1b37781b0f942be'
